622c41dbc7b0724bd43766edf7e16924fe08c039
Xinghao Huang <jhuang@brightedge.com>
2021-01-10 10:48:21 -0500

    django password generator

---
+++ code/python/django3/password_generator/generator/views.py
@@ -0,0 +1,30 @@
+import random
+
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    length = int(request.GET.get('length', 12))
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+    return render(request, 'generator/password.html', {'password': thepassword})
+
+
+def about(request):
+    return render(request, 'generator/about.html')
