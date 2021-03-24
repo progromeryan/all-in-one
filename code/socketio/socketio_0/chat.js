96880d4f64c8dbdee4bfa7a429d698a76cfe6264
Xinghao Huang <jhuang@brightedge.com>
2021-03-24 16:25:14 -0400

    406 408

--- code/socketio/socketio_0/chat.js
+++ code/socketio/socketio_0/chat.js
@@ -8,7 +8,11 @@ app.use(express.static(__dirname + "/public"));
 
 const expressServer = app.listen(9000);
 
-const io = socketio(expressServer);
+const io = socketio(expressServer, {
+  path: "/socket.io",
+  serveClient: true, // chat.html可以用socketio
+});
+
 io.on("connection", (socket) => {
   socket.emit("messageFromServer", { data: "Welcome to the socketio server" });
   socket.on("messageToServer", (dataFromClient) => {
