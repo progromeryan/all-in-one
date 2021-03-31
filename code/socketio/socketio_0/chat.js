ca37c75c50efad93a7c55de5dd523440dd626606
jacobhuang91@gmail.com <jacobhuang91@gmail.com>
2021-03-31 15:14:15 -0400

    socketio

--- code/socketio/socketio_0/chat.js
+++ code/socketio/socketio_0/chat.js
@@ -9,16 +9,17 @@ app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen(9000);
 
 const io = socketio(expressServer, {
-  path: "/socket.io",
-  serveClient: true, // chat.html可以用socketio
+    path: "/socket.io",
+    serveClient: true, // chat.html可以用socketio
 });
 
 io.on("connection", (socket) => {
-  socket.emit("messageFromServer", { data: "Welcome to the socketio server" });
-  socket.on("messageToServer", (dataFromClient) => {
-    console.log(dataFromClient);
-  });
-  socket.on("newMessageToServer", (msg) => {
-    io.emit("messageToClients", { text: msg.text });
-  });
+    socket.emit("messageFromServer", {data: "Welcome to the socketio server"});
+    socket.on("messageToServer", (dataFromClient) => {
+        console.log(dataFromClient);
+    });
+
+    socket.on("newMessageToServer", (msg) => {
+        io.emit("messageToClients", {text: msg.text});
+    });
 });
